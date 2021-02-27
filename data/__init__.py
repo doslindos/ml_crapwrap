648dd8627b553c8897393f6e25cf84b92ea6d43a
doslindos <juuso.m.lindholm@gmail.com>
2021-02-27 13:41:14 +0200

    configparser implemented and credentials switched to .ini file

--- data/__init__.py
+++ data/__init__.py
@@ -8,5 +8,5 @@ from importlib import import_module
 from numpy import array as nparray, float32 as npfloat32, append as npappend, save as npsave
 
 from .dataset_handler import DatasetHandler
-from utils.utils import duplicate_along, input_check, path_check
+from utils.utils import duplicate_along, input_check, path_check, get_credentials
 
