794355a0b14d46050df2ac60d18b6073d7a59af8
doslindos <juuso.m.lindholm@gmail.com>
2021-03-07 13:52:53 +0200

    Titanic data importing added

--- data/handlers/__init__.py
+++ data/handlers/__init__.py
@@ -1,4 +1,4 @@
-from .. import Path, jsonload
+from .. import Path, jsonload, jsondump
 from ..util.data_fetching import load_with_tfds_load, mysqldb_fetch, spotify_api_fetch
 from ..util.preprocessing import normalize_image, preprocess_spotify_features, preprocess_billboard
 from random import sample as rndsample
--- data/handlers/titanic/preprocess.py
+++ data/handlers/titanic/preprocess.py
@@ -1,5 +1,43 @@
-from .. import tfdata, rndsample, jsonload, Path
+from .. import tfdata, rndsample, jsonload, jsondump, Path
 from ...util.fetchers.kaggle.kaggle_fetcher import KaggleCompetitionDataFetcher
+
+from zipfile import ZipFile
+from csv import reader as csv_reader
+
+def resolve_type(column):
+    # Check if column has any digit strings
+    if any(i.isdigit() for i in column):
+        # Add zeros to empty values
+        column = ['0' if i == '' else i for i in column]
+        # Check if all values are ints
+        if all(i.isdigit() for i in column):
+            return (column, int)
+        else:
+            # Check if there are decimal points and that there are no empty space values
+            if any('.' in i for i in column) and not any(' ' in i for i in column):
+                # Change empty values to 0.0
+                column = ['0.0' if i == '' else i for i in column]
+                return (column, float)
+            else:
+                print("Somethings wrong! ", column)
+    # No numeric values
+    return (column, str)
+
+def change_types(data):
+    # Used to change datatypes in titanic data
+    # The dataset contains numeric values in strings
+    # Change numeric types from strings to numeric
+    
+    # Seperate by columns
+    columns = zip(*data)
+    for i, column in enumerate(columns):
+        column, i_type = resolve_type(column)
+        # If values were numeric change datatypes of the columns values
+        if i_type != str:
+            columns[i] = list(map(i_type, column))
+
+    # Pack columns back together and return the data
+    return list(zip(*columns))
 
 class DataPreprocessor(KaggleCompetitionDataFetcher):
 
@@ -13,7 +51,50 @@ class DataPreprocessor(KaggleCompetitionDataFetcher):
         print(self.save_folder)
         super()
 
+    def import_data(self):
+        titanic_data = ZipFile(Path("data", "titanic.zip"))
+        
+        # Read train data csv
+        with titanic_data.open("train.csv", 'r') as f:
+            lines = (line.decode("utf-8") for line in f)
+            train = [line for line in csv_reader(lines)]
+            # Assign columns and data
+            train_columns = train[0]
+            train_data = train[1:]
+        
+        # Read test data csv
+        with titanic_data.open("test.csv", 'r') as f:
+            lines = (line.decode("utf-8") for line in f)
+            test = [line for line in csv_reader(lines)]
+                
+            test_columns = test[0]
+            test_data = test[1:]
+
+        # Read gender data csv
+        with titanic_data.open("gender_submission.csv", 'r') as f:
+            lines = (line.decode("utf-8") for line in f)
+            genders = [line for line in csv_reader(lines)]
+            
+            gender_columns = genders[0]
+            gender_data = genders[1:]
+        
+        data = {
+            'train': (train_columns, train_data), 
+            'test': (test_columns, test_data),
+            'gender': (gender_columns, gender_data)
+            }
+            
+        # Save features dataset to a json file
+        with self.save_folder.joinpath('titanic.json').open('w', encoding='utf8') as f:
+            jsondump(data, f, ensure_ascii=False)
+                        
+
     def get_data(self, sample=None):
+        # Path to data
+        if not self.save_folder.joinpath("titanic.json").exists():
+            # Read files from .zip
+            self.import_data()
+            
         # Load ds
         self.dataset = jsonload(self.save_path.open("r", encoding='utf-8'))
         if sample is not None:
